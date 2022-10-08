65ab071d5b95d9a80642069643e433f0dac83d4b
retaEgoD <davidliang343@gmail.com>
2022-10-08 13:36:19 +1300

    Added hand selection module

---
+++ handSelect.c
@@ -0,0 +1,26 @@
+/** @file   handSelect.c
+    @author David Liang
+    @date   08/10/2022
+    @brief  Hand selection in rock paper scissors game using navstick.
+*/
+
+#include "system.h"
+#include "navswitch.h"
+#define ROCK navswitch_push_event_p (NAVSWITCH_SOUTH)
+#define PAPER navswitch_push_event_p (NAVSWITCH_NORTH)
+#define SCISSORS navswitch_push_event_p (NAVSWITCH_EAST)
+
+char selectHand(void)
+{
+    char handCharacter;
+    if (ROCK) {
+        handCharacter = 'R';
+    }
+    if (PAPER) {
+        handCharacter = 'P';
+    }
+    if (SCISSORS) {
+        handCharacter = 'S';
+    }
+    return handCharacter;
+}
