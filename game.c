97a4af631d78ffd1f3cc267e39ee5fba257a547b
retaEgoD <davidliang343@gmail.com>
2022-10-08 14:47:42 +1300

    Completed game.c to proof of concept level

--- game.c
+++ game.c
@@ -2,7 +2,11 @@
 #include "navswitch.h"
 #include "button.h"
 #include "ir_serial.h"
+#include "led.h"
 #include "handSelect.h"
+#include "main"
+
+#include <stdbool.h>
 
 int main (void)
 {
@@ -10,16 +14,26 @@ int main (void)
     navswitch_init();
     button_init();
     ir_serial_init();
+    led_init();
 
     char playerHand;
     char opponentHand;
+    ir_serial_ret receiveErrorCode;
 
     while (1) {
+
         while (!button_release_event_p(BUTTONS_NUM))   {
-            playerHand = seectHand();
+            playerHand = selectHand();
+            button_update();
         }
+
         ir_serial_transmit(playerHand);
+        receiveErrorCode = ir_serial_receive(&opponentHand);
 
-
+        if (receiveErrorCode == IR_SERIAL_OK) {
+            if (isWon(playerHand, opponentHand)) {
+                led_set(LED1, true);
+            }
+        }
     }
 }
